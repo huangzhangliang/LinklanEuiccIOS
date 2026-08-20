674b00c99f00d5fa77fa719e87f3acdba38f2b67
Hzl <82770536@qq.com>
2026-08-20 15:00:05 +0800

    no message

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "LinklanEuiccIOS",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "LinklanEuiccIOS",
+            targets: ["LinklanEuiccIOS"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "LinklanEuicc",
+            path: "Frameworks/LinklanEuicc.framework"
+        ),
+        .binaryTarget(
+            name: "eSIMManagerSDK",
+            path: "Frameworks/eSIMManagerSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "TYLPA",
+            path: "Frameworks/TYLPA.framework"
+        ),
+    ]
+)
